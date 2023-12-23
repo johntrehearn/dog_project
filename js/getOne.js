1fc240eb6bb324db4e5636d57249669f6c4afbb4
johntrehearn <johntrehearnbc@gmail.com>
2023-12-23 10:38:34 +0200

    one dog working

--- js/getOne.js
+++ js/getOne.js
@@ -1,57 +1,89 @@
 'use strict';
 
 (function () {
+  let keylist;
   let resultarea;
-  let inputField;
+  let searchvalue;
 
   document.addEventListener('DOMContentLoaded', init);
 
-  function init() {
+  async function init() {
+    keylist = document.getElementById('keylist');
     resultarea = document.getElementById('resultarea');
-    inputField = document.getElementById('get1id');
+    searchvalue = document.getElementById('searchvalue');
+
+    try {
+      const data = await fetch('/keys');
+      if (data.ok) {
+        const keys = await data.json();
+        if (keys.length > 0) {
+          populateList(keys);
+        } else {
+          showErrorMessage('search not available');
+        }
+      } else {
+        showErrorMessage('failed communication!');
+      }
+    } catch (err) {
+      showErrorMessage(err.message);
+    }
+  } //end of init
+
+  function populateList(keynames) {
+    for (const field of keynames) {
+      const option = document.createElement('option');
+      option.value = field;
+      option.textContent = field;
+
+      keylist.appendChild(option);
+    }
+
+    keylist.value = keynames[0];
 
     document.getElementById('submit').addEventListener('click', send);
-    inputField.addEventListener('focus', clear);
-  }
-
-  function clear() {
-    inputField.value = '';
-    resultarea.textContent = '';
-    resultarea.removeAttribute('class');
-  }
-
-  function updateStatus(status) {
-    resultarea.textContent = status.message;
-    resultarea.setAttribute('class', status.type);
-  }
+  } //end of populateList
 
   async function send() {
-    const value = inputField.value;
-    if (value <= 0) {
-      updateStatus({message: 'Invalid id', type: 'error'});
+    const keyName = keylist.value;
+    const value = searchvalue.value;
+
+    try {
+      const fetchOptions = {
+        method: 'POST',
+        body: JSON.stringify({value, key: keyName}),
+        headers: {'Content-Type': 'application/json'},
+      };
+      const data = await fetch('/search', fetchOptions);
+      const result = await data.json();
+
+      updatePage(result);
+    } catch (err) {
+      showErrorMessage(err.message);
+    }
+  }
+
+  function updatePage(data) {
+    if (!data) {
+      showErrorMessage('Programming error!');
+    } else if (data.length === 0) {
+      showErrorMessage('Nothing found');
     } else {
-        try {
-            const data = await fetch(`/all`);
-            const result = await data.json();
-      
-            const resultset = document.getElementById('resultset');
-      
-              const tr = document.createElement('tr');
-              tr.appendChild(createCell(dog.id));
-              tr.appendChild(createCell(dog.name));
-              tr.appendChild(createCell(dog.length));
-              tr.appendChild(createCell(dog.weightKg));
-              tr.appendChild(createCell(dog.breed));
-              resultset.appendChild(tr);
-            }
-          } catch (err) {
-            console.log(err);
-          }
-        } //end of init
-      
-        function createCell(data) {
-          const td = document.createElement('td');
-          td.textContent = data;
-          return td;
-        }
-      })();
+      const htmlString = data.map((item) => createPerson(item)).join(' ');
+      resultarea.innerHTML = htmlString;
+    }
+  }
+
+  function createPerson(dog) {
+    return `<div class="person">
+        <p>Number: ${dog.id}</p>
+        <p>First name: ${dog.name}</p>
+        <p>Last name: ${dog.length}</p>
+        <p>Department: ${dog.weightKg}</p>
+        <p>Salary: ${dog.breed}</p>
+    </div>`;
+  }
+
+  function showErrorMessage(message) {
+    resultarea.innerHTML = `<p>${message}</p>`;
+  }
+})();
